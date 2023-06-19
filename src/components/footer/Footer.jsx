c3b65cb9e410a0a352d4e997eeb15372f5e870e9
CasLinden <cas_linden@msn.com>
2023-06-19 15:31:09 +0900

    footer pic aspect ratio

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,42 +1,47 @@
-import "/src/css/Footer.css"
-import casWithComputer480 from "/src/assets/cas-with-computer-480px.jpg"
-import casWithComputer700 from "/src/assets/cas-with-computer-700px.jpg"
-import casWithComputer1200 from "/src/assets/cas-with-computer-1200px.jpg"
-import LinkBox from "../links/LinkBox"
+import "/src/css/Footer.css";
+import casWithComputer480 from "/src/assets/cas-with-computer-480px.jpg";
+import casWithComputer700 from "/src/assets/cas-with-computer-700px.jpg";
+import casWithComputer1200 from "/src/assets/cas-with-computer-1200px.jpg";
+import LinkBox from "../links/LinkBox";
 
-function Footer () {
-    return (
-        <div className="footer">
-            <div className="footer-top-wrapper">
-                <div className="contact-me">
-                    <h2>Contact Me</h2>
-                    <p> Please get in touch about working with me, I&apos;m excited to hear from you!</p>
-                    <p>Tokyo Suginami-ku <br />
-                    Hounan 1-50-7 168-0062
-                    </p>
-                    <p>
-                    070-4072-8944 <br />
-                    cas_linden@msn.com
-                    </p>
-                </div>
-                <LinkBox></LinkBox>
-            </div>
-            <div className="footer-image">
-                <img 
-                    src={casWithComputer480}
-                    srcSet={`
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-top-wrapper">
+        <div className="contact-me">
+          <h2>Contact Me</h2>
+          <p>
+            Please get in touch about working with me, I&apos;m excited to hear
+            from you!
+          </p>
+          <p>
+            168-0062 <br />
+            Tokyo, Suginami-Ku <br />
+            Hounan 1-50-7
+          </p>
+          <p>
+            Phone: 070-4072-8944 <br />
+            Mail: cas_linden@msn.com
+          </p>
+        </div>
+        <LinkBox></LinkBox>
+      </div>
+      <div className="footer-image">
+        <img
+          src={casWithComputer480}
+          srcSet={`
                         ${casWithComputer480} 480w, 
                         ${casWithComputer700} 700w, 
                         ${casWithComputer1200} 1200w
                     `}
-                    sizes="(max-width: 480px) 480px, 
+          sizes="(max-width: 480px) 480px, 
                            (max-width: 700px) 700px, 
                            1200px"
-                    alt="picture of cas working on a laptop computer" 
-                />
-            </div>
-        </div>
-    )
+          alt="picture of cas working on a laptop computer"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
